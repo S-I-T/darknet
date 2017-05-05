be07057a30881b23a5755f9c104e0e8b99c7efa2
pabgonza <pabgonza@gmail.com>
2017-05-05 16:58:45 -0300

    Rutinas de entrenamiento con 2 clases a partir de yolo-voc

---
+++ train/yolo-voc-pablo_2c/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PREFIX=yolo-voc-pablo_2c
+BASE_DIR=/root/src/darknet
+TRAIN_DIR=/root/src/darknet/train/$PREFIX
+
+DATA_FILE=$TRAIN_DIR/${PREFIX}.data
+CFG_FILE=$TRAIN_DIR/${PREFIX}.cfg
+WEIGHTS_FILE=/root/models/train/${PREFIX}/${PREFIX}_8000.weights
+TEST_DIR=$TRAIN_DIR/test
+
+cd $BASE_DIR
+
+for FILE in $TEST_DIR/*.jpg
+do
+  echo "Procesando $f..."
+  ./darknet detector test $DATA_FILE $CFG_FILE $WEIGHTS_FILE $FILE
+  mv predictions.png ${PREFIX}_${FILE##*/}.png
+done
+
+
+
---
+++ train/yolo-voc-pablo_2c/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PREFIX=yolo-voc-pablo_2c
+BASE_DIR=/root/src/darknet
+TRAIN_DIR=/root/src/darknet/train/$PREFIX
+
+DATA_FILE=$TRAIN_DIR/${PREFIX}.data
+CFG_FILE=$TRAIN_DIR/${PREFIX}.cfg
+WEIGHTS_FILE=/root/models/public/darknet19_448.conv.23
+
+
+cd $BASE_DIR
+
+./darknet detector train $DATA_FILE $CFG_FILE $WEIGHTS_FILE
+
