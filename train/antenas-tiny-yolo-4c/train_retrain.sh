03409ab85067415f412f7c22dbdb99d202f91745
pabgonza <pabgonza@gmail.com>
2017-05-26 12:11:32 -0400

    Se sube entrenamiento para dataset de Antenas

---
+++ train/antenas-tiny-yolo-4c/train_retrain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PREFIX=antenas-tiny-yolo-4c
+BASE_DIR=/root/src/darknet
+TRAIN_DIR=/root/src/darknet/train/$PREFIX
+
+DATA_FILE=$TRAIN_DIR/${PREFIX}.data
+CFG_FILE=$TRAIN_DIR/${PREFIX}.cfg
+WEIGHTS_FILE=/root/models/train/${PREFIX}/${PREFIX}_8000.weights
+
+cd $BASE_DIR
+
+./darknet detector train $DATA_FILE $CFG_FILE $WEIGHTS_FILE
+
