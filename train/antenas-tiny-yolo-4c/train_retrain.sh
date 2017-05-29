c817618ad7af5bb9b6312c99144de831293b1479
pabgonza <pabgonza@gmail.com>
2017-05-29 10:15:39 -0400

    nuevo checkpoint entrenamiento antenas

--- train/antenas-tiny-yolo-4c/train_retrain.sh
+++ train/antenas-tiny-yolo-4c/train_retrain.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 PREFIX=antenas-tiny-yolo-4c
 BASE_DIR=/root/src/darknet
 TRAIN_DIR=/root/src/darknet/train/$PREFIX
 
 DATA_FILE=$TRAIN_DIR/${PREFIX}.data
 CFG_FILE=$TRAIN_DIR/${PREFIX}.cfg
-WEIGHTS_FILE=/root/models/train/${PREFIX}/${PREFIX}_8000.weights
+WEIGHTS_FILE=/root/models/train/${PREFIX}/${PREFIX}_10000.weights
+
 
 cd $BASE_DIR
 
