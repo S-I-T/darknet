6417429f94f9230c4e356b8c2eb87e51d75e16e2
pabgonza <pabgonza@gmail.com>
2017-06-08 15:53:23 -0400

    Pruebas con tiny-yolo v1

---
+++ train/antenas-yolov1-tiny-yolo-4c/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PREFIX=antenas-yolov1-tiny-yolo-4c
+BASE_DIR=/root/src/darknet
+TRAIN_DIR=/root/src/darknet/train/$PREFIX
+
+DATA_FILE=$TRAIN_DIR/${PREFIX}.data
+CFG_FILE=$TRAIN_DIR/${PREFIX}.cfg
+WEIGHTS_FILE=/root/models/public/darknet.conv.weights
+
+
+cd $BASE_DIR
+
+./darknet detector train $DATA_FILE $CFG_FILE $WEIGHTS_FILE
+
